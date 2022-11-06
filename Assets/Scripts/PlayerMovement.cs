e2673e4a826d43d10e1c4db6d007136d6962f8bb
Rebix <78218707+Reebix@users.noreply.github.com>
2022-11-07 00:46:56 +0100

    initial commit?

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Http.Headers;
+using UnityEditor;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Object References")]
+    public CharacterController controller;
+    public new Camera camera;
+    public StatsManager statsManager;
+    [Space(10)]
+    [Header("Movement")]
+    public float dpi = 100; 
+    public float moveSpeed = 5f;
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+     Cursor.lockState = CursorLockMode.Locked;
+     Cursor.visible = false;
+    }
+    
+
+    float _xRotation;
+    // Update is called once per frame
+    void Update()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveY = Input.GetAxis("Vertical");
+        
+        float mouseX = Input.GetAxis("Mouse X") * dpi * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * dpi * Time.deltaTime;
+
+      
+        _xRotation -= mouseY;
+        _xRotation = Mathf.Clamp(_xRotation, -90f, 90f);
+        Vector3 move = transform.right * moveX + transform.forward * moveY;
+        
+        controller.Move(move * (Time.deltaTime * moveSpeed * statsManager.speedMultiplier));
+        controller.transform.Rotate(Vector3.up * (mouseX));
+       
+        Quaternion rotation = controller.transform.rotation.normalized;
+        camera.transform.rotation = (Quaternion.Euler(_xRotation, rotation.eulerAngles.y, rotation.eulerAngles.z));
+
+        
+    }
+}
